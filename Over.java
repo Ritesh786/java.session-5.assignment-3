3138632c24ba470f012f8dbd1934bec4bea547e6
Ritesh786 <jharitesh6@gmail.com>
2016-07-26 17:59:20 +0530

    assignment 3
    assignment 3

---
+++ Over.java
@@ -0,0 +1,11 @@
+package com.Ritesh.tools;
+
+public class Over {
+	public Over (double a) {
+		System.out.println("Area of Square = "+ a*a);
+}
+	
+public Over (double l,double b){
+	System.out.println("Area of rectangle = " +l*b);
+	}
+}
---
+++ TestOver.java
@@ -0,0 +1,14 @@
+package com.Ritesh.test;
+
+import com.Ritesh.tools.Over;
+
+public class TestOver {
+
+	public static void main(String[] args) {
+		Over obj=new Over(5.0);
+		Over sbj=new Over(5.0,9.0);
+		
+
+	}
+
+}
